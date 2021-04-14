bea1f7e6d0b06b2ec652848b7a4295f750cc62e0
lntechnical2 <82169706+lntechnical2@users.noreply.github.com>
2021-04-14 15:36:16 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+from telegram import Update
+from telegram.ext import Updater , CommandHandler, CallbackQueryHandler, CallbackContext,Filters,MessageHandler
+
+updater = Updater( "BOT_TOKEN" ,use_context = True )
+
+def start(updater,context):
+ updater.message.reply_text('''Hi iam welcome messanger bot 
+Add me to your group 
+ 
+ Made with Love ❤️ by @lntechnical
+
+  ''')
+def help(updater,context):
+ updater.message.reply_text("Add me to your group ")
+ 
+
+def add_group(update: Update, context: CallbackContext):
+    for member in update.message.new_chat_members:
+        update.message.reply_text(f'Hello {member.full_name} , Welcome to ln support Thank you for Joining  ')
+
+add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
+updater.dispatcher.add_handler(add_group_handle)
+
+dp =updater.dispatcher.add_handler
+dp(CommandHandler('start',start))
+dp(CommandHandler('help',help))
+
+updater.start_polling()
+updater.idle()
