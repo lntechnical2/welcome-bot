15f856808181118eb5251bc28e0a382c298a4046
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-04-14 17:11:33 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Updater , CommandHandler, CallbackQueryHandler, CallbackContext,Filters,MessageHandler
 import os
 
-Token =os.environ.get("BOT_TOKEN",None)
+Token =os.environ.get("BOT_TOKEN")
 updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
