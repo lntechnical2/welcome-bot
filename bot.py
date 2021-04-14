1c037dbbbaa0b15df241945018c5910cdc4e7a61
lntechnical2 <82169706+lntechnical2@users.noreply.github.com>
2021-04-14 15:46:18 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from telegram import Update
 from telegram.ext import Updater , CommandHandler, CallbackQueryHandler, CallbackContext,Filters,MessageHandler
+import os
 
-updater = Updater( "BOT_TOKEN" ,use_context = True )
+Token =os.environ.get(BOT_TOKEN)
+updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
  updater.message.reply_text('''Hi iam welcome messanger bot 
