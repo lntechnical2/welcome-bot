bd9dfe598593b0ce499e9d8d95c48e491edbcd09
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-05-06 13:21:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,16 +6,14 @@ Token =os.environ.get("BOT_TOKEN",None)
 updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('''Made with Love ❤️ by @bisnuray
-
-  ''')
+ updater.message.reply_text('Made This is welcome bot \n with Love ❤️ by @bisnuray')
 def help(updater,context):
  updater.message.reply_text("Add me to your group ")
  
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} , Thanks you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} \n How are you ? ')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
