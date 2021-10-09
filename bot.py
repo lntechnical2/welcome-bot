ec0dbf5687fa36867f8ddaef6025c1c0e0c29f41
MayilGaming <90180469+MayilGaming@users.noreply.github.com>
2021-10-09 10:43:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ def start(updater,context):
  updater.message.reply_text('''Hi iam welcome messanger bot 
 Add me to your group 
  
- Made with Love ❤️ by @lntechnical
+ Made with Love ❤️ by @mayilmovies
 
   ''')
 def help(updater,context):
@@ -18,7 +18,7 @@ def help(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} , Welcome to ln support Thank you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} , Welcome to our group Thank you for Joining  ')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
