d673875d1bcc345e217626d76d737f89d6657366
comaliking <99741204+comaliking@users.noreply.github.com>
2022-02-15 20:41:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,11 @@ updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
  updater.message.reply_text('''Hi iam welcome messanger bot 
-Add me to your group 
+Add Me To Your Group 👇👇
+
+http://t.me/iam_comali_bot?startgroup=true
  
- Made with Love ❤️ by @lntechnical
+ Made with Love ❤️ by @subeesh005
 
   ''')
 def help(updater,context):
@@ -18,7 +20,7 @@ def help(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} , Welcome to ln support Thank you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} , Welcome To The group Thank You For Joining  ')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
