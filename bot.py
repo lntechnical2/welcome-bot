771cf663cdf79e09a8943bd38316afbd7e5fc4b1
Subeesh005 <97532153+Subeesh005@users.noreply.github.com>
2022-09-01 09:35:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ def start(updater,context):
  updater.message.reply_text('''Hi iam welcome messanger bot 
 Add me to your group 
  
- Made with Love ❤️ by @lntechnical
+ Made with Love ❤️ by @subeesh005
 
   ''')
 def help(updater,context):
